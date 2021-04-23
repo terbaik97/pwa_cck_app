8c852ba27591250afde598a7231c65b074b9e785
Muhamad Aziim Abdul Halim <aziimmashoor1997@gmail.com>
2021-04-23 20:15:29 +0800

    Change user profile

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApiService } from '../services/api.service';
+import { AuthService } from '../services/auth.service';
+import { ProfileService } from '../services/profile.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit{
+  profile_data: any;
+  nickname: any;
+  buttonLogout = true;
+  constructor(
+    private _api : ApiService, 
+    private _auth: AuthService, 
+    private route: ActivatedRoute,
+    private _profileService: ProfileService,
+    private router: Router, 
+    
+  ) {
+    this.nickname = this._auth.getUserNickname();
+  }
+  ngOnInit() {
+    console.log(this._auth.getUserNickname());
+    this._profileService.getProfile(this._auth.getUserEmail()).subscribe((res: any) =>{
+      console.log(res);
+      this.profile_data = res.data;
+  })
+  }
 
-  constructor() {}
-
+  logout(){
+    // logout and reset the jwt token
+    localStorage.removeItem('jwt');
+    localStorage.removeItem('nickname');
+    localStorage.removeItem('email');
+    this.router.navigate(['/']) 
+  }
 }
