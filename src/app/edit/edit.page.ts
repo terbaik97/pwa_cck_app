1d11d0fde67b4675cc4328226ff56c535a1d8a3f
Muhamad Aziim Abdul Halim <aziimmashoor1997@gmail.com>
2021-04-23 20:24:59 +0800

    Fix edit page

--- src/app/edit/edit.page.ts
+++ src/app/edit/edit.page.ts
@@ -24,6 +24,11 @@ export class EditPage implements OnInit {
   public poi: any;
   public poiData: any[];
   public index: any;
+  message ="";
+  data: any;
+  updatedata: any;
+  adddata: any;
+  poi_id: any;
   constructor(private formBuilder: FormBuilder, private _poiService: PoiService, private activatedRoute: ActivatedRoute,
     private router: Router,) {
     
@@ -32,13 +37,15 @@ export class EditPage implements OnInit {
       if (this.router.getCurrentNavigation().extras.state) {
         this.index = this.router.getCurrentNavigation().extras.state.index;
         this.poiData = JSON.parse(localStorage.getItem("addpoiData"));
-        this.poi = this.poiData[this.index];
+        this.poi =[ this.poiData[this.index]];
       }
     })
 
     this.requiredInfo = formBuilder.group({
-      placeName: ['', Validators.required],
-      categoryName: [''],
+      name: ['', Validators.required],
+      category: [''],
+      poi_latitude:[''],
+      poi_longitude:['']
      
       });
 
@@ -46,7 +53,7 @@ export class EditPage implements OnInit {
     this.additionalInfo = formBuilder.group(
      
       {
-      player1: ['', Validators.required],
+      field: ['', Validators.required],
       
       }
     );
@@ -57,16 +64,39 @@ export class EditPage implements OnInit {
   
 
   ngOnInit() {
-     this._poiService.getCategories().subscribe(data => {
+     this._poiService.getCategories().subscribe((data: any) => {
+      
+      if (data === ""){
+        this.categoryData = [];
+      }
       this.categoryData = data;
-      console.log(this.categoryData);
-      console.log(this.categoryData[0]["category_name"]);
-      })
-    
-      console.log("check data")
-      console.log(this.poi.latitude);
+      });
      
-    
+      this._poiService.getPoibyCoordinate(this.poi).subscribe((res: any) => {
+          
+          console.log(res)
+          if (res){
+            this.updatedata = true;
+            this.data = res;
+            console.log(this.data)
+            this.poi_id = this.data.id
+            this.requiredInfo.patchValue({
+              name: this.data.name,
+              poi_latitude:  this.data.poi_latitude,
+              poi_longitude:  this.data.poi_longitude
+            });
+          }
+        
+        },(err: any) =>{
+          console.log(err.error) 
+          this.adddata = true;
+          this.requiredInfo.patchValue({
+            name: "",
+            poi_latitude:  this.poi[0].latitude,
+            poi_longitude:  this.poi[0].longitude
+          });
+        }
+        );
 
   }
   
@@ -75,7 +105,7 @@ export class EditPage implements OnInit {
  
   addControl(){
     this.playerCount++;
-    this.additionalInfo.addControl('player' + this.playerCount, new FormControl('', Validators.required));
+    this.additionalInfo.addControl('field' + this.playerCount, new FormControl('', Validators.required));
   }
   removeControl(control){
     this.additionalInfo.removeControl(control.key);
@@ -90,16 +120,45 @@ export class EditPage implements OnInit {
          alert("Name required")
         } 
         else {
-          console.log("success!")
-          // console.log(this.requiredInfo.value);
-          // console.log(this.additionalInfo.value);
-          this.poiData =[].concat(this.requiredInfo.value,this.additionalInfo.value);
-          console.log(this.poiData);
-          let message = this._poiService.saveData(this.poiData);
-          console.log(message);
+         
+          // let coordinate = [this.poi.latitude,this.poi.longitude]
+          this.poiData = [].concat(this.requiredInfo.value,this.additionalInfo.value);
+          console.log(this.data);
+          if(this.data === ""){
+            console.log(this.poiData);
+          }
+          else{
+            console.log("this.poiData");
+          }
+           this._poiService.saveData(this.poiData).subscribe((res: any) => { 
+            if(res){ 
+              console.log(res.message);
+              this.message = res.message
+              this.router.navigate(['']) 
+            } 
+          }, err => { 
+            console.log(err) 
+            this.message = err
+          });
+         
+        
         }
       }
 
+  update(poi_id: any){
+    console.log(poi_id);
+    this.poiData = [].concat(this.requiredInfo.value,this.additionalInfo.value,poi_id);
+    this._poiService.updateData(this.poiData).subscribe((res: any) => { 
+      if(res){ 
+        console.log(res.message);
+        this.message = res.message
+        this.router.navigate(['/']) 
+      } 
+    }, err => { 
+      console.log(err) 
+      this.message = err
+    });
+  }
       
 }
 
