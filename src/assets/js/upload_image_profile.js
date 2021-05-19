373ca32b02345233ae844befc22499254cb4a02d
terbaik97 <aziimmashoor1997@gmail.com>
2021-05-19 21:54:41 +0800

    Fix CSS and add feature upload image for profile page

--- src/assets/js/upload_image_profile.js
+++ src/assets/js/upload_image_profile.js
@@ -1,35 +1,24 @@
-/*  ==========================================
-    SHOW UPLOADED IMAGE
-* ========================================== */
-function readURL(input) {
-    if (input.files && input.files[0]) {
-      var reader = new FileReader();
+$(document).ready(function() {
+
+    
+  var readURL = function(input) {
+      if (input.files && input.files[0]) {
+          var reader = new FileReader();
+
+          reader.onload = function (e) {
+              $('.profile-pic').attr('src', e.target.result);
+          }
   
-      reader.onload = function(e) {
-        $('#imageResult')
-          .attr('src', e.target.result);
-      };
-      reader.readAsDataURL(input.files[0]);
-    }
+          reader.readAsDataURL(input.files[0]);
+      }
   }
   
-  $(function() {
-    $('#upload').on('change', function() {
-      readURL(input);
-    });
+
+  $(".file-upload").on('change', function(){
+      readURL(this);
   });
   
-  /*  ==========================================
-      SHOW UPLOADED IMAGE NAME
-  * ========================================== */
-  var input = document.getElementById('upload');
-  var infoArea = document.getElementById('upload-label');
-  
-  input.addEventListener('change', showFileName);
-  
-  function showFileName(event) {
-    var input = event.srcElement;
-    var fileName = input.files[0].name;
-    infoArea.textContent = 'File name: ' + fileName;
-  }
-  
+  $(".upload-button").on('click', function() {
+     $(".file-upload").click();
+  });
+});
